986b3e2f83bff84fa2f1e56a3b23b9bc2bd87678
DarkMindFX <contact@darkmindfx.com>
2020-11-01 12:20:30 +0100

    File storage access

---
+++ Fineo/Common/Fineo.Interfaces/FileInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fineo.Interfaces
+{
+    public class FileInfo
+    {
+        string Path
+        {
+            get;
+            set;
+        }
+
+        long Size
+        {
+            get;
+            set;
+        }
+
+        byte[] Content
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Fineo/Common/Fineo.Interfaces/IFileStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fineo.Interfaces
+{
+    public interface IFileStorageParams
+    {
+        Dictionary<string, string> Parameters
+        {
+            get;
+            set;
+        }
+    }
+
+    public interface IFileStorage
+    {
+        void Init(IFileStorageParams initParams);
+
+        FileInfo Download(FileInfo fileInfo);
+
+        bool Upload(FileInfo fileInfo);
+
+        IFileStorageParams CreateParams();
+    }
+}
